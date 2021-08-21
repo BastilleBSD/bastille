e763b93975ac615ebefef1d51e862292ba2157f6
AverageHumanoid <krith@tuta.io>
2021-08-21 19:54:15 +0000

    3rd approach

---
+++ usr/local/share/bastille/hosts.sh
@@ -0,0 +1,68 @@
+#!/bin/sh
+#
+# Copyright (c) 2018-2021, Christer Edwards <christer.edwards@gmail.com>
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+# * Neither the name of the copyright holder nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+. /usr/local/share/bastille/common.sh
+. /usr/local/etc/bastille/bastille.conf
+
+usage() {
+    error_exit "Usage: bastille hosts TARGET"
+}
+
+# Handle special-case commands first.
+case "$1" in
+help|-h|--help)
+    usage
+    ;;
+esac
+
+if [ $# -ne 0 ]; then
+    usage
+fi
+
+for _jail in ${JAILS}; do
+        ## resolve hostname to jail IP address
+        HOSTNAME=$(bastille config ${_jail} get host.hostname)
+        HOSTSFILE="${bastille_jailsdir}/${_jail}/root/etc/hosts"
+        EXISTINGENTRY=$(grep '^10\|^172\|^192' "${HOSTSFILE}")
+
+        if [ "$(bastille config ${_jail} get vnet)" = 'enabled' ]; then
+                IP4ADDR=$(jexec -l ${_jail} ifconfig -n vnet0 inet | sed -n "/.inet /{s///;s/ .*//;p;}")
+        else
+                IP4ADDR=$(bastille config ${_jail} get ip4.addr)
+        fi
+
+        UPDATEENTRY="${IP4ADDR} ${HOSTNAME}"
+
+        if [ -z "${EXISTINGENTRY}" ]; then
+                sed -i '' "15s/^.*/${UPDATEENTRY}/" "${HOSTSFILE}"
+        else
+                sed -i '' "15s/^10.*/${UPDATEENTRY}/; 15s/^172.*/${UPDATEENTRY}/; 15s/^192.*/${UPDATEENTRY}/" "${HOSTSFILE}"
+        fi
+done
