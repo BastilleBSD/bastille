7a5223d8c9d04b8e94701680426bd14e6360f711
tschettervictor <85497460+tschettervictor@users.noreply.github.com>
2024-12-18 19:51:09 -0700

    Update top.sh

--- usr/local/share/bastille/top.sh
+++ usr/local/share/bastille/top.sh
@@ -29,26 +29,29 @@
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 . /usr/local/share/bastille/common.sh
+. /usr/local/etc/bastille/bastille.conf
 
 usage() {
     error_exit "Usage: bastille top TARGET"
 }
 
 # Handle special-case commands first.
-case "$1" in
-help|-h|--help)
-    usage
-    ;;
+case "${1}" in
+    help|-h|--help)
+        usage
+        ;;
 esac
 
-if [ $# -ne 0 ]; then
+if [ $# -ne 1 ]; then
     usage
 fi
 
-bastille_root_check
+TARGET="${1}"
 
-for _jail in ${JAILS}; do
-    info "[${_jail}]:"
-    jexec -l "${_jail}" /usr/bin/top
-    echo -e "${COLOR_RESET}"
-done
+bastille_root_check
+set_target_single "${TARGET}"
+check_target_is_running "${TARGET}"
+
+info "[${TARGET}]:"
+jexec -l "${TARGET}" /usr/bin/top
+echo -e "${COLOR_RESET}"
