10b84bbe122ccd8c509124eea4661adea7079660
tschettervictor <85497460+tschettervictor@users.noreply.github.com>
2024-12-18 19:41:58 -0700

    Update clone.sh

--- usr/local/share/bastille/clone.sh
+++ usr/local/share/bastille/clone.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 #
-# Copyright (c) 2018-2024, Christer Edwards <christer.edwards@gmail.com>
+set -x
+# Copyright (c) 2018-2023, Christer Edwards <christer.edwards@gmail.com>
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -28,168 +29,148 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-COLOR_RED=
-COLOR_GREEN=
-COLOR_YELLOW=
-COLOR_RESET=
+. /usr/local/share/bastille/common.sh
+. /usr/local/etc/bastille/bastille.conf
 
-bastille_root_check() {
-    if [ "$(id -u)" -ne 0 ]; then
-        ## permission denied
-        error_notify "Bastille: Permission Denied"
-        error_exit "root / sudo / doas required"
-    fi
+usage() {
+    error_exit "Usage: bastille config TARGET get|set propertyName [newValue]"
 }
 
-enable_color() {
-    . /usr/local/share/bastille/colors.pre.sh
-}
+# we need jail(8) to parse the config file so it can expand variables etc
+print_jail_conf() {
 
-# If "NO_COLOR" environment variable is present, or we aren't speaking to a
-# tty, disable output colors.
-if [ -z "${NO_COLOR}" ] && [ -t 1 ]; then
-    enable_color
+    # we need to pass a literal \n to jail to get each parameter on its own
+    # line
+    jail -f "$1" -e '
+'
+} 
+
+# Handle special-case commands first.
+case "$1" in
+help|-h|--help)
+    usage
+    ;;
+esac
+
+if [ $# -eq 2 ] || [ $# -gt 4 ]; then
+    usage
 fi
 
-# Notify message on error, but do not exit
-error_notify() {
-    echo -e "${COLOR_RED}$*${COLOR_RESET}" 1>&2
-}
+bastille_root_check
 
-# Notify message on error and exit
-error_exit() {
-    error_notify "$@"
-    exit 1
-}
+TARGET="${1}"
+ACTION="${2}"
+shift 2
 
-info() {
-    echo -e "${COLOR_GREEN}$*${COLOR_RESET}"
-}
+set_target "${TARGET}"
 
-warn() {
-    echo -e "${COLOR_YELLOW}$*${COLOR_RESET}"
-}
-
-check_target_exists() {
-    local _TARGET="${1}"
-    if [ -d "${bastille_jailsdir}"/"${_TARGET}" ]; then
-        return 0
-    else
-        error_exit "Jail not found \"${_TARGET}\""
-    fi
-}
-
-check_target_is_running() {
-    local _TARGET="${1}"
-  if [ ! "$(/usr/sbin/jls name | awk "/^${_TARGET}$/")" ]; then
-      error_exit "[${_TARGET}]: Not started. See 'bastille start ${_TARGET}'."
-  fi
-}
-
-check_target_is_stopped() {
-    local _TARGET="${1}"
-    if [ "$(/usr/sbin/jls name | awk "/^${TARGET}$/")" ]; then
-        error_exit "${TARGET} is running. See 'bastille stop ${TARGET}'."
-    fi
-}
-
-set_target() {
-    if [ "${1}" = ALL ] || [ "${1}" = all ]; then
-        target_all_jails
-    else
-        check_target_exists "${1}"
-        JAILS="${1}"
-        export JAILS
-    fi
-}
-
-set_target_single() {
-    if [ "${1}" = ALL ] || [ "${1}" = all ]; then
-        error_exit "[all|ALL] not supported with this command."
-    else
-        check_target_exists "${1}"
-        TARGET="${1}"
-        export TARGET
-    fi
-}
-
-target_all_jails() {
-    local _JAILS=$(/usr/sbin/jls name)
-    JAILS=""
-    for _jail in ${_JAILS}; do
-        _JAILPATH=$(/usr/sbin/jls -j "${_jail}" path)
-        if [ -z ${_JAILPATH##${bastille_jailsdir}*} ]; then
-            JAILS="${JAILS} ${_jail}"
+case ${ACTION} in
+    get)
+        if [ $# -ne 1 ]; then
+            error_notify 'Too many parameters for a "get" operation.'
+            usage
         fi
-        export JAILS
-    done
-}
+        ;;
+    set) ;;
+    *) error_exit 'Only get and set are supported.' ;;
+esac
 
-generate_vnet_jail_netblock() {
-    local jail_name="$1"
-    local use_unique_bridge="$2"
-    local external_interface="$3"
-    ## determine number of containers + 1
-    ## iterate num and grep all jail configs
-    ## define uniq_epair
-    local jail_list="$(bastille list jails)"
-    if [ -n "${jail_list}" ]; then
-        local list_jails_num="$(echo "${jail_list}" | wc -l | awk '{print $1}')"
-        local num_range=$((list_jails_num + 1))
-        for _num in $(seq 0 "${num_range}"); do
-            if ! grep -q "e[0-9]b_bastille${_num}" "${bastille_jailsdir}"/*/jail.conf; then
-                if ! grep -q "epair${_num}" "${bastille_jailsdir}"/*/jail.conf; then
-                    local uniq_epair="bastille${_num}"
-                    local uniq_epair_bridge="${_num}"
-                    break
-                fi
+PROPERTY=${1}
+shift
+VALUE="$@"
+
+for _jail in ${JAILS}; do
+    FILE="${bastille_jailsdir}/${_jail}/jail.conf"
+    if [ ! -f "${FILE}" ]; then
+        error_notify "jail.conf does not exist for jail: ${_jail}"
+        continue
+    fi
+
+    if [ "${ACTION}" = 'get' ]; then
+        _output=$(
+            print_jail_conf "${FILE}" | awk -F= -v property="${PROPERTY}" '
+                $1 == property {
+                    # note that we have found the property
+                    found = 1;
+                    # check if there is a value for this property
+                    if (NF == 2) {
+                        # remove any quotes surrounding the string
+                        sub(/^"/, "", $2);
+                        sub(/"$/, "", $2);
+                        print $2;
+                    } else {
+                        # no value, just the property name
+                        print "enabled";
+                    }
+                    exit 0;
+                }
+                END {
+                    # if we have not found anything we need to print a special
+                    # string
+                    if (! found) {
+                        print("not set");
+                        #  let the caller know that this is a warn condition
+                        exit(120);
+                    }
+                }'
+            )
+        # check if our output is a warning or regular
+        if [ $? -eq 120 ]; then
+            warn "${_output}"
+        else
+            echo "${_output}"
+        fi
+    else # Setting the value. -- cwells
+        if [ -n "${VALUE}" ]; then
+            VALUE=$(echo "${VALUE}" | sed 's/\//\\\//g')
+            if echo "${VALUE}" | grep ' ' > /dev/null 2>&1; then # Contains a space, so wrap in quotes. -- cwells
+                VALUE="'${VALUE}'"
             fi
-        done
-    else
-        local uniq_epair="bastille0"
-        local uniq_epair_bridge="0"
-    fi
-    if [ -n "${use_unique_bridge}" ]; then
-        ## generate bridge config
-        cat <<-EOF
-  vnet;
-  vnet.interface = "e${uniq_epair_bridge}b_${jail_name}";
-  exec.prestart += "ifconfig epair${uniq_epair_bridge} create";
-  exec.prestart += "ifconfig ${external_interface} addm epair${uniq_epair_bridge}a";
-  exec.prestart += "ifconfig epair${uniq_epair_bridge}a up name e${uniq_epair_bridge}a_${jail_name}";
-  exec.prestart += "ifconfig epair${uniq_epair_bridge}b up name e${uniq_epair_bridge}b_${jail_name}";
-  exec.poststop += "ifconfig ${external_interface} deletem e${uniq_epair_bridge}a_${jail_name}";
-  exec.poststop += "ifconfig e${uniq_epair_bridge}a_${jail_name} destroy";
-EOF
-    else
-        ## generate config
-        cat <<-EOF
-  vnet;
-  vnet.interface = e0b_${uniq_epair};
-  exec.prestart += "jib addm ${uniq_epair} ${external_interface}";
-  exec.prestart += "ifconfig e0a_${uniq_epair} description \"vnet host interface for Bastille jail ${jail_name}\"";
-  exec.poststop += "jib destroy ${uniq_epair}";
-EOF
-    fi
-}
+            LINE="  ${PROPERTY} = ${VALUE};"
+        else
+            LINE="  ${PROPERTY};"
+        fi
 
-checkyesno() {
-    ## copied from /etc/rc.subr -- cedwards (20231125)
-    ## issue #368 (lowercase values should be parsed)
-    ## now used for all bastille_zfs_enable=YES|NO tests
-    ## example: if checkyesno bastille_zfs_enable; then ...
-    ## returns 0 for enabled; returns 1 for disabled
-    eval _value=\$${1}
-    case $_value in
-    [Yy][Ee][Ss]|[Tt][Rr][Uu][Ee]|[Oo][Nn]|1)
-        return 0
-        ;;
-    [Nn][Oo]|[Ff][Aa][Ll][Ss][Ee]|[Oo][Ff][Ff]|0)
-        return 1
-        ;;
-    *)
-        warn "\$${1} is not set properly - see rc.conf(5)."
-        return 1
-        ;;
-    esac
-}
+        # add the value to the config file, replacing any existing value or, if
+        # there is none, at the end
+        #
+        # awk doesn't have "inplace" editing so we use a temp file
+        _tmpfile=$(mktemp) || error_exit "unable to set because mktemp failed"
+        cp "${FILE}" "${_tmpfile}" && \
+        awk -F= -v line="${LINE}" -v property="${PROPERTY}" '
+            BEGIN {
+                # build RE as string as we can not expand vars in RE literals
+                prop_re = "^[[:space:]]*" property "[[:space:]]*$";
+            }
+            $1 ~ prop_re && !found {
+                # we already have an entry in the config for this property so
+                # we need to substitute our line here rather than keep the
+                # existing line
+                print(line);
+                # note we have already found the property
+                found = 1;
+                # move onto the next line
+                next;
+            }
+            $1 == "}" {
+                # reached the end of the stanza so if we have not already
+                # added our line we need to do so now
+                if (! found) {
+                    print(line);
+                }
+            }
+            {
+                # print each uninteresting line unchanged
+                print;
+            }
+        ' "${_tmpfile}" > "${FILE}"
+        rm "${_tmpfile}"
+    fi
+done
+
+# Only display this message once at the end (not for every jail). -- cwells
+if [ "${ACTION}" = 'set' ]; then
+    info "A restart is required for the changes to be applied. See 'bastille restart ${TARGET}'."
+fi
+
+exit 0
