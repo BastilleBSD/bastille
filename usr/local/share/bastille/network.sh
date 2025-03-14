daa19bd152fa0bf71498723858294e4ca528fe5d
tschettervictor <85497460+tschettervictor@users.noreply.github.com>
2025-03-13 22:27:54 -0600

    network: Typo

--- usr/local/share/bastille/network.sh
+++ usr/local/share/bastille/network.sh
@@ -41,7 +41,7 @@ usage() {
     -C | --classic              Add an interface to a classic (non-VNET) jail.
     -M | --static-mac           Generate a static MAC address for the interface.
     -n | --no-ip                Create interface without an IP (VNET only).
-    -P | --passthrough          Pass the entire interface througg to the jail.
+    -P | --passthrough          Pass the entire interface through to the jail.
     -V | --vnet                 Add a VNET interface to an existing jail.
     -v | --vlan VLANID          Add interface with specified VLAN ID (VNET only).
     -x | --debug                Enable debug mode.
