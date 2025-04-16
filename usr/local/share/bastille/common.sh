741d2c6e12aeb6004c97bb3808c1f8e8584e274b
tschettervictor <85497460+tschettervictor@users.noreply.github.com>
2025-04-16 16:10:09 -0600

    fix shellcheck

--- usr/local/share/bastille/common.sh
+++ usr/local/share/bastille/common.sh
@@ -121,7 +121,7 @@ check_target_is_stopped() {
 get_epair_count() {
     for _config in /usr/local/etc/bastille/*.conf; do
         local bastille_jailsdir="$(sysrc -f "${_config}" -n bastille_jailsdir)"
-        local _epair_list="$(printf '%s\n' $( (grep -Eos '(e[0-9]+b|bastille[0-9]+)' ${bastille_jailsdir}/*/jail.conf; ifconfig -g epair) | grep -Eo "[0-9]+") ${_epair_list} | sort -u)"
+        local _epair_list="$(printf '%s\n' "$( (grep -Eos '(e[0-9]+b|bastille[0-9]+)' ${bastille_jailsdir}/*/jail.conf; ifconfig -g epair) | grep -Eo "[0-9]+")" "${_epair_list}" | sort -u)"
     done
     _epair_count="$(echo ${_epair_list} | wc -l | awk '{print $1}')"
     export _epair_count
