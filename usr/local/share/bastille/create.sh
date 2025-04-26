61918796305221e185516def616d2be31b6d881d
tschettervictor <85497460+tschettervictor@users.noreply.github.com>
2025-04-26 11:04:09 -0600

    Update create.sh

--- usr/local/share/bastille/create.sh
+++ usr/local/share/bastille/create.sh
@@ -540,9 +540,7 @@ create_jail() {
                     if [ -f /usr/share/examples/jails/jng ] && [ ! -f /usr/local/bin/jng ]; then
                         install -m 0544 /usr/share/examples/jails/jng /usr/local/bin/jng
                     fi
-                fi
-            else 
-                error_exit "[ERROR]: 'bastille_network_vnet_type' not set properly: ${bastille_network_vnet_type}"
+                fi 
             fi
         fi
     elif [ -n "${LINUX_JAIL}" ]; then
