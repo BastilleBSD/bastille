b45f03a9fecef536c794e6b588cdb7738ece8653
tschettervictor <85497460+tschettervictor@users.noreply.github.com>
2025-07-22 11:44:11 -0600

    common: show warning on all jail updates

--- usr/local/share/bastille/common.sh
+++ usr/local/share/bastille/common.sh
@@ -560,6 +560,11 @@ update_jail_syntax_v1() {
     # Only apply if old syntax is found
     if grep -Eoq "exec.prestart.*ifconfig epair[0-9]+ create.*" "${jail_config}"; then
 
+        warn "\n[WARNING]\n"
+        warn "Updating jail.conf file..."
+        warn "Please review your jail.conf file after completion."
+        warm "VNET jails created without -M will be assigned a new MAC address."
+
         if [ "$(echo -n "e0a_${jail}" | awk '{print length}')" -lt 16 ]; then
             local new_host_epair=e0a_${jail}
             local new_jail_epair=e0b_${jail}
