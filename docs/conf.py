a7d2f0a011eb79c7f4dce3bdf861d062dc9ceb83
tschettervictor <tschetter.victor@gmail.com>
2025-12-14 17:07:37 -0700

    Version 1.3.0

--- docs/conf.py
+++ docs/conf.py
@@ -5,9 +5,9 @@ copyright = '2018-2025, Christer Edwards'
 author = 'Christer Edwards'
 
 # The short X.Y version
-version = '1.2.2'
+version = '1.3.0'
 # The full version, including alpha/beta/rc tags
-release = '1.2.2.251204'
+release = '1.3.0.251214'
 
 # -- General configuration ---------------------------------------------------
 
