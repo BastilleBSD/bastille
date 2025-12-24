efe336b0ee7704b80eaaeb9f17e6189e8898f10e
tschettervictor <85497460+tschettervictor@users.noreply.github.com>
2025-12-23 22:32:37 -0700

    monitor: make sure jail is running

--- usr/local/share/bastille/monitor.sh
+++ usr/local/share/bastille/monitor.sh
@@ -121,7 +121,7 @@ for jail in ${JAILS}; do
     bastille_jail_monitor="${bastille_jailsdir}/${jail}/monitor"
 
     ## iterate service(s) and check service status; restart on failure
-    if ! check_target_is_running "${jail}" && [ -z "${ACTION}" ] && [ -f "${bastille_jail_monitor}" ]; then
+    if check_target_is_running "${jail}" && [ -z "${ACTION}" ] && [ -f "${bastille_jail_monitor}" ]; then
         for service in $(xargs < "${bastille_jail_monitor}"); do
             ## check service status
             if ! jexec -l -U root "${jail}" service "${service}" status >/dev/null 2>/dev/null; then
