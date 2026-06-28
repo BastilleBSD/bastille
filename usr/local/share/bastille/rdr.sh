cbce21bb766a3baa25bf6f6f5779bb251bb424e3
tschettervictor <tschetter.victor@gmail.com>
2026-06-28 12:43:16 -0600

    rdr: fix logged rules

--- usr/local/share/bastille/rdr.sh
+++ usr/local/share/bastille/rdr.sh
@@ -81,6 +81,8 @@ check_jail_validity() {
     # Check if rdr-anchor is defined in pf.conf
     if ! (pfctl -sn | grep rdr-anchor | grep 'rdr/\*' >/dev/null); then
         error_exit "[ERROR]: rdr-anchor not found in pf.conf"
+    elif ! (pfctl -sn | grep anchor | grep 'bastille/\*' >/dev/null); then
+        error_exit "[ERROR]: bastille filtering achor not found in pf.conf"
     fi
 }
 
@@ -229,24 +231,32 @@ load_rdr_log_rule() {
     # shellcheck disable=SC2193
     if { [ "${inet}" = "ipv4" ] || [ "${inet}" = "dual" ]; } then
         if ! ( pfctl -a "rdr/${TARGET}" -Psn;
-            printf '%s\nrdr pass %s on $%s inet proto %s from %s to %s port %s -> %s port %s\n' "$if" "$log" "${bastille_network_pf_ext_if}" "$proto" "$src" "$dst" "$host_port" "$JAIL_IP" "$jail_port" ) \
+            printf '%s\nrdr %s on $%s inet proto %s from %s to %s port %s -> %s port %s\n' "$if" "$log" "${bastille_network_pf_ext_if}" "$proto" "$src" "$dst" "$host_port" "$JAIL_IP" "$jail_port" ) \
             | pfctl -a "rdr/${TARGET}" -f-; then
             error_exit "[ERROR]: Failed to create logged IPv4 rdr rule \"${if_name} ${src} ${dst} ${proto} ${host_port} ${jail_port}\""
-        else
-            info 2 "IPv4 ${proto}/${host_port}:${jail_port} on ${if_raw}"
         fi
+        if ! ( pfctl -a "bastille/${TARGET}" -Psn 2>/dev/null;
+            printf '%s\npass in %s on $%s inet proto %s from %s to %s port %s\n' "$if" "$log" "${bastille_network_pf_ext_if}" "$proto" "$src" "$JAIL_IP" "$jail_port" ) \
+            | pfctl -a "bastille/${TARGET}" -f-; then
+            error_exit "[ERROR]: Failed to create logged IPv4 filter rule \"${if_name} ${src} ${dst} ${proto} ${host_port} ${jail_port}\""
+        fi
+        info 2 "IPv4 ${proto}/${host_port}:${jail_port} on ${if_raw}"
     fi
 
     # Create IPv6 rdr rule with log (if ip6.addr is enabled)
     # shellcheck disable=SC2193
     if [ -n "${JAIL_IP6}" ] && { [ "${inet}" = "ipv6" ] || [ "${inet}" = "dual" ]; } then
         if ! ( pfctl -a "rdr/${TARGET}" -Psn;
-            printf '%s\nrdr pass %s on $%s inet6 proto %s from %s to %s port %s -> %s port %s\n' "$if" "$log" "${bastille_network_pf_ext_if}" "$proto" "$src" "$dst" "$host_port" "$JAIL_IP6" "$jail_port" ) \
+            printf '%s\nrdr %s on $%s inet6 proto %s from %s to %s port %s -> %s port %s\n' "$if" "$log" "${bastille_network_pf_ext_if}" "$proto" "$src" "$dst" "$host_port" "$JAIL_IP6" "$jail_port" ) \
             | pfctl -a "rdr/${TARGET}" -f-; then
             error_exit "[ERROR]: Failed to create logged IPv6 rdr rule \"${if_name} ${src} ${dst} ${proto} ${host_port} ${jail_port}\""
-        else
-            info 2 "IPv6 ${proto}/${host_port}:${jail_port} on ${if_raw}"
         fi
+        if ! ( pfctl -a "bastille/${TARGET}" -Psn 2>/dev/null;
+            printf '%s\npass in %s on $%s inet6 proto %s from %s to %s port %s\n' "$if" "$log" "${bastille_network_pf_ext_if}" "$proto" "$src" "$JAIL_IP6" "$jail_port" ) \
+            | pfctl -a "bastille/${TARGET}" -f-; then
+            error_exit "[ERROR]: Failed to create logged IPv6 filter rule \"${if_name} ${src} ${dst} ${proto} ${host_port} ${jail_port}\""
+        fi
+        info 2 "IPv6 ${proto}/${host_port}:${jail_port} on ${if_raw}"
     fi
 }
 
@@ -330,6 +340,8 @@ shift
 
 bastille_root_check
 set_target_single "${TARGET}"
+
+info 1 "[${TARGET}]:"
 
 while [ "$#" -gt 0 ]; do
     case "${1}" in
