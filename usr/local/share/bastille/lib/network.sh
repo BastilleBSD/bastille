defb3f7916718936c18bb9de5428c1e62c8a9142
tschettervictor <tschetter.victor@gmail.com>
2026-08-17 09:26:00 -0600

    dont use SHARED/NAT

--- usr/local/share/bastille/lib/network.sh
+++ usr/local/share/bastille/lib/network.sh
@@ -230,11 +230,6 @@ validate_ip() {
                     error_exit "[ERROR]: Invalid IP: ${ip4}"
                 fi
             done
-            if route -n get "${ip4}" | grep -q "gateway"; then
-                export STANDARD_JAIL_NAT=1
-            else
-                export STANDARD_JAIL_SHARED=1
-            fi
             info 1 "\nValid IP: ${ip4}"
             export IP4_ADDR="${ip4}"
         else
